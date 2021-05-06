78f0c9bf13b7ce966b161208359dabe238e4104c
Denis Yudin <denistu2191@gmail.com>
2021-05-06 15:29:20 +0000

    Added systemd unit to running  bot

--- bot.py
+++ bot.py
@@ -1,29 +1,30 @@
 import os
-import schedule
 import time
+import schedule
 from instapy import InstaPy
 from instapy import smart_run
+from instapy import set_workspace
 
 insta_username = os.environ.get('IG_USERNAME', 'username')
 insta_password = os.environ.get('IG_PASSWORD', 'password')
 friendlist = os.environ['IG_FRIENDLIST'].split(',')
 
+set_workspace(path=os.path.dirname(os.path.realpath(__file__)))
 
-# """
-# Like last new posts from friendlists
-# """
 def like():
-    friends = InstaPy(username=insta_username, password=insta_password, headless_browser=True,)
-    with smart_run(friends, threaded=True):
+    bot = InstaPy(username=insta_username, password=insta_password, headless_browser=True)
+    with smart_run(bot, threaded=True):
         print('💞 Showing friends some love 💖')
-        friends.set_relationship_bounds(enabled=False)
-        friends.set_skip_users(skip_private=False)
-        friends.set_do_like(True,percentage=100)
-        friends.interact_by_users(friendlist,amount=1,randomize=False)
+        bot.set_relationship_bounds(enabled=False)
+        bot.set_skip_users(skip_private=False)
+        bot.set_do_like(True,percentage=100)
+        bot.interact_by_users(friendlist,amount=1,randomize=False)
 
-        
-if __name__ == '__main__':
-    schedule.every(1).minutes.do(like)
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+
+print('-------------------------------------')
+print(f'User: {insta_username}')
+print(f'UserList: {friendlist}')
+schedule.every(1).minutes.do(like)
+while True:
+    schedule.run_pending()
+    time.sleep(60)
